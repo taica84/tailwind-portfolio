52fabd7ccc88063a4eec7e230a4652e7d9137d44
taica84 <taica_rules@yahoo.com>
2022-09-12 11:55:39 +0300

    change title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed" >
       <div>
-      <h1 className='text-5xl ml-2 font-signature' >My    Portfolio: 1:24</h1>
+      <h1 className='text-5xl ml-2 font-signature' >Mihai</h1>
       </div>
         
       <ul className='hidden md:flex' >
